6e7c350a565f6f1525e6dd07cbda287f138f21de
Arlene Pelenda <arlenej011@gmail.com>
2021-03-30 22:05:33 -0300

    verify error connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const configExpress = require('./config/configExpress');
+const conexao = require('./infra/conexao');
 
 const app = configExpress();
 
-app.listen(3000, () => {
-  console.log('servidor na porta 3000')
+conexao.connect( error => {
+  if(error){
+    //para que não execute o resto do código
+    throw error;
+  }
+  
+  app.listen(3000, () => {
+    console.log('servidor na porta 3000')
+  });
 
 });
+
