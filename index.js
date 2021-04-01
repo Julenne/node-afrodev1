601d1bc0a8053450b9aca0ae11cc5418b3840206
Arlene Pelenda <arlenej011@gmail.com>
2021-03-31 22:06:36 -0300

    Fix index bugs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const configExpress = require('./config/configExpress');
 const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/Tabelas');
 
-const app = configExpress();
-
-conexao.connect( error => {
+conexao.connect(error => {
   if(error){
     //para que não execute o resto do código
     throw error;
   }
-  
+  Tabelas.init(conexao);
+
+  app = configExpress();
+
   app.listen(3000, () => {
     console.log('servidor na porta 3000')
   });
