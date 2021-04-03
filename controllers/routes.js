d44303dd39aef2593d17da5b03cea6134b537dbb
Arlene Pelenda <arlenej011@gmail.com>
2021-04-02 21:25:40 -0300

    Add delete route

--- controllers/routes.js
+++ controllers/routes.js
@@ -13,4 +13,9 @@ module.exports = app => {
     const id = parseInt(req.params.id)
     Agendamento.buscaPorId(id, resp)
   })
+
+  app.delete('/agendamentos',(req, resp) => {
+    const id = parseInt(req.params.id)
+    Agendamento.remover(id, resp)
+  })
 };
